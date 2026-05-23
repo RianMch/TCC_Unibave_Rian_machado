0a09fb93249413de5720bb704dbcacb4e14cfaa9
Rianmch <huehue123.hz@gmail.com>
2026-05-23 07:14:54 -0300

    fix: corrigido caminho auth

--- backend/index.ts
+++ backend/index.ts
@@ -1,28 +1,31 @@
-import express = require("express");
-import cors = require("cors");
-import cookieParser = require("cookie-parser");
-import bodyParser = require("body-parser");
-import dotenv = require("dotenv");
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import authRouter from "./src/router/auth.js";
+
 dotenv.config();
 
-
-const app =express();
-const port=process.env.PORT||3000;
+const app = express();
+const port = process.env["PORT"] ?? 3000;
 
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
+  origin: "http://localhost:5173",
+  credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
-app.get('/api/status',(req,res)=>{
-    res.json({
-        status:'ok',
-        mensagem:'servidor rodando'
-    });
+
+app.use("/api/auth", authRouter);
+
+app.get("/api/status", (req, res) => {
+  res.json({
+    status: "ok",
+    mensagem: "servidor rodando"
+  });
 });
 
-app.listen(port,()=>{
-    console.log(`servidor rodando na porta ${port}`);
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
